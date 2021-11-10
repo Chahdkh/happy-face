b73f17c5083f7e41d4a00b02ce7caa4d66c99098
Chahdkh <94078328+Chahdkh@users.noreply.github.com>
2021-11-10 21:08:21 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showNumber(1)
+basic.showNumber(2)
+basic.showNumber(3)
+basic.showNumber(4)
+basic.showNumber(5)
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
